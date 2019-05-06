febf06d1243ee9b2fbfa58f82fed0668d45f8253
wangjie <wangjie3@haima.me>
2019-05-06 10:19:25 +0800

    info in master

--- app/src/main/java/cm/jiege/testgitcommand/MainActivity.kt
+++ app/src/main/java/cm/jiege/testgitcommand/MainActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+    private var text:String = "这是在master分支上"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
