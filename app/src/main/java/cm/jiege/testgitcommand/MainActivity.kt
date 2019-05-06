c5ae24132205919678e98c148ecb2720a6a09c18
wangjie <wangjie3@haima.me>
2019-05-06 10:27:17 +0800

    info in temp

--- app/src/main/java/cm/jiege/testgitcommand/MainActivity.kt
+++ app/src/main/java/cm/jiege/testgitcommand/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private var text:String = "这是在temp分支上"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
