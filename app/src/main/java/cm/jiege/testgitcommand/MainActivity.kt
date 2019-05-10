4ca0241c9a55cde5e660f6b3f6e6b4ce748fb7d1
wangjie <wangjie3@haima.me>
2019-05-10 18:20:15 +0800

    创建branch-1分支后，第二次修改text

--- app/src/main/java/cm/jiege/testgitcommand/MainActivity.kt
+++ app/src/main/java/cm/jiege/testgitcommand/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     private var text:String = "这是在branch-1分支上"+"第一次修改"
+    private var text2:String = "这是在branch-1分支上"+"第二次修改"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
